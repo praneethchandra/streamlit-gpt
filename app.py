66f22e3640fcc401499b8deff38360bff999d7ef
pkgone <praneethchandra@gmail.com>
2023-06-24 22:26:26 -0700

    parse stock report to show dividends

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.ticker as ticker
 
+def clean_data(file):
+    data = pd.read_csv(file, names=range(10))
+    # data = data.drop('0', axis=1)
+    data.columns = data.iloc[0]
+    data = data[1:]
+
+    data['Quantity'] = data['Quantity'].astype(float)
+
+    data['Price'] = data['Price'].str.replace('$','')
+    data['Price'] = data['Price'].str.replace('(','')
+    data['Price'] = data['Price'].str.replace(')','')
+    data['Price'] = data['Price'].astype(float)
+    data['Amount'] = data['Amount'].str.replace('(','')
+    data['Amount'] = data['Amount'].str.replace('$','')
+    data['Amount'] = data['Amount'].str.replace(')', '')
+    data['Amount'] = data['Amount'].str.replace("'", '')
+    data['Amount'] = data['Amount'].str.replace(",", '')
+    data['Amount'] = data['Amount'].astype(float)
+    return data[data["Trans Code"] == "CDIV"]
 
 def main():
     st.title("Hello, World! EDA Streamlit App")
@@ -13,9 +32,9 @@ def main():
     data_file = st.file_uploader("Upload CSV", type=["csv"])
 
     if data_file is not None:
-        data = pd.read_csv(data_file)
+        data = clean_data(data_file)
         st.write("Data overview:")
-        st.write(data.head())
+        st.write(data.head(50))
 
         st.sidebar.header("Visualizations")
         plot_options = ["Bar plot", "Scatter plot", "Histogram", "Box plot"]
@@ -24,20 +43,22 @@ def main():
         if selected_plot == "Bar plot":
             x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
             y_axis = st.sidebar.selectbox("Select y-axis", data.columns)
+            hue_col = st.sidebar.selectbox("Select hue", data.columns)
             st.write("Bar plot:")
             fig, ax = plt.subplots()
-            sns.barplot(x=data[x_axis], y=data[y_axis], ax=ax)
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=10))
+            sns.barplot(x=data[x_axis], y=data[y_axis], hue=data[hue_col], ax=ax)
+            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=30))
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
             st.pyplot(fig)
         
         elif selected_plot == "Scatter plot":
             x_axis = st.sidebar.selectbox("Select x-axis", data.columns)
             y_axis = st.sidebar.selectbox("Select y-axis", data.columns)
+            hue_col = st.sidebar.selectbox("Select hue", data.columns)
             st.write("Scatter plot:")
             fig, ax = plt.subplots()
-            sns.scatterplot(x=data[x_axis], y=data[y_axis], ax=ax)
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=10))
+            sns.scatterplot(x=data[x_axis], y=data[y_axis], hue=data[hue_col], ax=ax)
+            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=30))
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
             st.pyplot(fig)
 
@@ -47,7 +68,7 @@ def main():
             st.write("Histogram")
             fig, ax = plt.subplots()
             sns.histplot(data[column], bins=bins, ax=ax)
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=10))
+            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=30))
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
             st.pyplot(fig)
 
@@ -56,7 +77,7 @@ def main():
             st.write("Box plot")
             fig, ax = plt.subplots()
             sns.boxplot(data[column], ax=ax)
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=10))
+            ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True, nbins=30))
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
             st.pyplot(fig)
 
